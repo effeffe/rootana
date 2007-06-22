2c294eef1d235286fde80243ea64ede7e8f9fe7c
Konstantin Olchanski <olchansk>
2007-06-22 13:56:31 +0000

    Fixes against invalid XML ODB data Implement (non-functional) odbReadInt()
    SVN revision: 26

--- XmlOdb.cxx
+++ XmlOdb.cxx
@@ -18,11 +18,34 @@
 
 #include "XmlOdb.h"
 
-XmlOdb::XmlOdb(const char*buf,int bufLength) //ctor
+XmlOdb::XmlOdb(const char*xbuf,int bufLength) //ctor
 {
   fParser = new TDOMParser();
   fParser->SetValidate(false);
+
+  char*buf = (char*)malloc(bufLength);
+  memcpy(buf, xbuf, bufLength);
+  for (int i=0; i<bufLength; i++)
+    if (!isascii(buf[i]))
+      buf[i] = 'X';
+    else if (buf[i]=='\n')
+      0;
+    else if (buf[i]=='\r')
+      0;
+    else if (!isprint(buf[i]))
+      buf[i] = 'X';
+    else if (buf[i] == 0x1D)
+      buf[i] = 'X';
+
+  char* xend = strstr(buf,"odb>");
+  if (xend)
+    xend[4] = 0;
+
+  //printf("end: %s\n", buf+bufLength-5);
+
   fParser->ParseBuffer(buf,bufLength);
+  //TXmlDocument* xxx = fParser->GetXMLDocument();
+  //assert(xxx);
   fOdb = FindNode(fParser->GetXMLDocument()->GetRootNode(),"odb");
   assert(fOdb);
 }
@@ -136,10 +159,10 @@ TXMLNode* XmlOdb::FindPath(TXMLNode*node,const char* path)
     {
       // skip leading slashes
       while (*path == '/')
-	path++;
+        path++;
       
       if (*path == 0)
-	return node;
+        return node;
       
       const int kElemSize = 256;
       char elem[kElemSize+1];
@@ -148,51 +171,67 @@ TXMLNode* XmlOdb::FindPath(TXMLNode*node,const char* path)
       // copy the next path element into "elem"-
       // copy "path" until we hit "/" or end of string
       for (int i=0; i<kElemSize; i++)
-	{
-	  if (*path==0 || *path=='/')
-	    break;
-	  elem[i] = *path++;
-	}
+        {
+          if (*path==0 || *path=='/')
+            break;
+          elem[i] = *path++;
+        }
       
       //printf("looking for \"%s\" more \"%s\"\n",elem,path);
       
       for (; node != NULL; node = node->GetNextNode())
-	{
-	  const char* nodename = node->GetNodeName();
-	  const char* namevalue = GetAttrValue(node,"name");
+        {
+          const char* nodename = node->GetNodeName();
+          const char* namevalue = GetAttrValue(node,"name");
 	  
-	  //printf("node name: \"%s\", \"name\" value: \"%s\"\n",node->GetNodeName(),namevalue);
+          //printf("node name: \"%s\", \"name\" value: \"%s\"\n",node->GetNodeName(),namevalue);
 	  
-	  bool isDir = strcmp(nodename,"dir") == 0;
-	  bool isKey = strcmp(nodename,"key") == 0;
+          bool isDir = strcmp(nodename,"dir") == 0;
+          bool isKey = strcmp(nodename,"key") == 0;
+          bool isKeyArray = strcmp(nodename,"keyarray") == 0;
 	  
-	  if (!isKey && !isDir)
-	    continue;
+          if (!isKey && !isDir && !isKeyArray)
+            continue;
 	  
-	  //
-	  // compare directory names
-	  //
+          //
+          // compare directory names
+          //
 	  
-	  if (strcasecmp(elem,namevalue) == 0)
-	    {
-	      if (isDir)
-		{
-		  // found the right subdirectory, descend into it
-		  node = node->GetChildren();
-		  break;
-		}
-	      else if (isKey)
-		{
-		  return node;
-		}
-	    }
-	}
+          if (strcasecmp(elem,namevalue) == 0)
+            {
+              if (isDir)
+                {
+                  // found the right subdirectory, descend into it
+                  node = node->GetChildren();
+                  break;
+                }
+              else if (isKey || isKeyArray)
+                {
+                  return node;
+                }
+            }
+        }
     }
 }
 
 int      XmlOdb::odbReadAny(   const char*name, int index, int tid,void* value)    { assert(!"Not implemented!"); }
-int      XmlOdb::odbReadInt(   const char*name, int index, int      defaultValue)  { assert(!"Not implemented!"); }
 uint32_t XmlOdb::odbReadUint32(const char*name, int index, uint32_t defaultValue)  { assert(!"Not implemented!"); }
+
+int      XmlOdb::odbReadInt(   const char*name, int index, int      defaultValue)
+{
+  TXMLNode *node = FindPath(NULL,name);
+  if (!node)
+    return defaultValue;
+  const char* typevalue = GetAttrValue(node,"type");
+  if (strcasecmp(typevalue,"INT") != 0)
+    return defaultValue;
+  const char* text = node->GetText();
+  printf("for %s, type is %s, text is %s\n", name, typevalue, text);
+  DumpTree(node);
+  exit(1);
+  return 0;
+}
+
 bool     XmlOdb::odbReadBool(  const char*name, int index, bool     defaultValue)
 {
   TXMLNode *node = FindPath(NULL,name);
