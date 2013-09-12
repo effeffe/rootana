ed57b1fe227acb6ffa6e78ed9f7f03ed1f97f8fb
Thomas Lindner <lindner>
2013-09-12 16:22:35 +0000

    Add support for having multiple events worth of V1190 data in a single bank.  Information on whether there are multiple events in this bank can be accessed with following new functions:
      /// Get the number of events in this bank
      int TV1190Data::GetEventsInBank(){return fGlobalHeader.size();};
    
      /// Get Event Index; this is which event number within the bank.
      uint32_t TDCMeasurement::GetEventIndex() const{return event_index;};
    
    
    
    
    SVN revision: 142

--- libAnalyzer/TV1190Data.cxx
+++ libAnalyzer/TV1190Data.cxx
@@ -47,23 +47,35 @@ TV1190Data::TV1190Data(int bklen, int bktype, const char* name, void *pdata):
     TGenericData(bklen, bktype, name, pdata)
 {
   
+  // Do decoding.  Decoding is complicated by the fact that there can be 
+  // multiple events in the same bank.  So need to find and save multiple
+  // event headers, trailers.
+
+  
   // Do some sanity checking.  
   // Make sure first word has right identifier
   if( (GetData32()[0] & 0xf8000000) != 0x40000000) 
     std::cerr << "First word has wrong identifier; first word = 0x" 
 	      << std::hex << GetData32()[0] << std::dec << std::endl;
-  fGlobalHeader = GetData32()[0];
-
 
   // Scan through the data, saving information as we go.
   int found_trailer_word = -1;
-  fExtendedTriggerTimeTag = -1;
+  //  fExtendedTriggerTimeTag = -1;
 
   uint32_t current_tdc_header =0;
   int index_for_trailer = 0;
   int index_for_error = 0;
+  int numberEventsInBank = 0;
+  fWordCountTotal =0;
   for(int i = 0; i < GetSize(); i++){
     uint32_t word = GetData32()[i];
+    //std::cout << "0x"<<std::hex << word << std::dec << std::endl;
+
+    // Header
+    if( (word & 0xf8000000) == 0x40000000){
+      fGlobalHeader.push_back(word);
+      numberEventsInBank++;
+    }
 
     // TDC header
     if( (word & 0xf8000000) == 0x08000000){
@@ -71,7 +83,7 @@ TV1190Data::TV1190Data(int bklen, int bktype, const char* name, void *pdata):
     }
     // TDC measurement
     if( (word & 0xf8000000) == 0x00000000){
-      fMeasurements.push_back(TDCMeasurement(current_tdc_header,word));
+      fMeasurements.push_back(TDCMeasurement(current_tdc_header,word,numberEventsInBank-1));
     }
     
     // TDC trailer
@@ -97,25 +109,34 @@ TV1190Data::TV1190Data(int bklen, int bktype, const char* name, void *pdata):
 
 
     // Extended Trigger Time Tag
-    if( (word & 0xf8000000) == 0x88000000) fExtendedTriggerTimeTag = (word & 0x07ffffff);
+    if( (word & 0xf8000000) == 0x88000000) fExtendedTriggerTimeTag.push_back((int)(word & 0x07ffffff));
 
     // Found global trailer
     if( (word & 0xf8000000) == 0x80000000){ // Found trailer
       found_trailer_word = i;
-      fWordCount = (word & 0x0001fffe0) >> 5;
-      if(fWordCount != GetSize())
-	std::cerr << "Error in decoding V1190 data; word count in trailer ("<<fWordCount 
-		  << ") doesn't match bank size ("<< GetSize() << ")." << std::endl;
-      fStatus = (word & 0x07000000) >> 24;
+      fWordCountTotal += (word & 0x0001fffe0) >> 5;      
+      fStatus.push_back( (word & 0x07000000) >> 24);
     }
  
   }
-  
-  if(found_trailer_word == -1)
+
+  if(found_trailer_word == -1){
     std::cerr << "Error in decoding V1190 data; didn't find trailer." << std::endl;
+    std::cerr << "Bank dump: " << std::endl;
+    for(int i = 0; i < GetSize(); i++){
+      uint32_t word = GetData32()[i];
+      std::cout << "0x"<<std::hex << word << std::dec << std::endl;
+    }
+  }
   if(found_trailer_word != GetSize()-1)
     std::cerr << "Error; did not find trailer on last word of bank. trailer word = " << found_trailer_word
 	      << " last word = " << GetSize()-1 << std::endl;
+
+  if(fWordCountTotal != GetSize()){
+    std::cerr << "Error in decoding V1190 data; word count in all trailers ("<<fWordCountTotal 
+	      << ") doesn't match bank size ("<< GetSize() << ")." << std::endl;
+  }
+
 
 }
 
@@ -123,7 +144,7 @@ void TV1190Data::Print(){
 
   std::cout << "V1190 decoder for bank " << GetName().c_str() << std::endl;
   std::cout << "event counter = " << GetEventCounter() << ", geographic address: " <<  GetGeoAddress() << std::endl;
-  
+  std::cout << "Number of events in this bank: " << GetEventsInBank() << std::endl;
   for(unsigned int i = 0; i < fMeasurements.size(); i++){
     std::cout << "Measurement: " <<    fMeasurements[i].GetMeasurement() << " for tdc/chan "  <<
       fMeasurements[i].GetTDCNumber() << "/"<< fMeasurements[i].GetChannel();
@@ -133,7 +154,7 @@ void TV1190Data::Print(){
       std::cout << " (trailing edge meas)";
 
     std::cout << "[event_id = " << fMeasurements[i].GetEventID() << ",bunch_id=" 
-	      << fMeasurements[i].GetBunchID()<< "]";
+	      << fMeasurements[i].GetBunchID()<< "]" << "Event index=" << fMeasurements[i].GetEventIndex();
     if(fMeasurements[i].HasTDCErrorWord())
       std::cout << "[errors=0x"<< std::hex << fMeasurements[i].GetErrors() << std::dec << "]";
     std::cout << std::endl;
