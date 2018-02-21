7603a7e01690b075bc29ce38f0c4b688dab1e3b7
Thomas Lindner <lindner@triumf.ca>
2018-02-21 11:05:32 -0800

    Clean up the endian byte swapping; previous code didn't work on Macos.

--- libAnalyzer/TRB3Decoder.cxx
+++ libAnalyzer/TRB3Decoder.cxx
@@ -7,10 +7,26 @@ void Trb3Calib::SetTRB3LinearCalibrationConstants(float low_value, float high_va
   trb3LinearHighEnd = high_value;
 }
 
+// Get endian byte swapping code; use built-in code for non-MacOS Linux
+#if defined(OS_LINUX) && !defined(OS_DARWIN)
 #include <byteswap.h>
+#else
+/* Swap bytes in 32 bit value.  */
+#define R__bswap_constant_32(x) \
+     ((((x) & 0xff000000) >> 24) | (((x) & 0x00ff0000) >>  8) |               \
+      (((x) & 0x0000ff00) <<  8) | (((x) & 0x000000ff) << 24))
+#endif
 
 TrbDecoder::TrbDecoder(int bklen, void *pdata, std::string bankname){
-
+  
+  fPacketSize = 0;
+  fDecoding = 0;
+  fBoardId = 0;
+  fSeqNr = 0;
+  fRunNr = 0;
+  fDate = 0;
+  fTime = 0;
+  
   // New decoder, for data read directly from UDP socket
   if(bankname != std::string("TRB0")){
     uint32_t* fData = reinterpret_cast<uint32_t*>(pdata);
@@ -19,8 +35,11 @@ TrbDecoder::TrbDecoder(int bklen, void *pdata, std::string bankname){
     // fourth word is endian encoding check word
     if(!((fData[3] & 0x1) == 1 && (fData[3] & 0x80000000) == 0)){
       for(int i = 0; i < bklen/4; i++){
+#if defined(OS_LINUX) && !defined(OS_DARWIN)
 	fData[i] = __bswap_32 (fData[i]);
-	if(0)std::cout << "Flip/flop: " << i << " " << std::hex << fData[i] << std::dec << std::endl;
+#else
+        fData[i] = R__bswap_constant_32(fData[i]);
+#endif
       }
     }
     int size_subevent = fData[2]/4;
@@ -54,9 +73,13 @@ TrbDecoder::TrbDecoder(int bklen, void *pdata, std::string bankname){
 	pointer++;
 	uint32_t tdcWord = fData[pointer];
 	
-	if((fpgaWord & 0xf) > 3 )
+	if((fpgaWord & 0xf) > 3 ){
 	  std::cout << "TDC FPGA ID? " << std::hex << fpgaWord << " " << headerWord 
-		    << " " << tdcWord << std::dec << std::endl; 
+		    << " " << tdcWord << std::dec << std::endl;
+          for(int i = 0; i < 10; i++)
+            std::cout << i << " 0x"<<std::hex
+                      << fData[i] << std::dec << std::endl;
+        }
 	if((tdcWord & 0xe0000000) == 0x80000000){
 	  fMeasurements.push_back(TrbTdcMeas(fpgaWord, headerWord,
 					     epochWord, tdcWord));
